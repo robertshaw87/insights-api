e687ab490d36323451ed5575078c7894c736aa7c
Robert Shaw <robertshaw87@gmail.com>
2018-09-20 03:43:31 -0700

    create mode helper

--- utility/statistics.js
+++ utility/statistics.js
@@ -26,15 +26,36 @@ statistics.median = (array, field) => {
 
 // Takes in an array of posts and the key to find the mode of
 // The mode is returned as either a value or an array of values if there's more than one mode
-
+statistics.mode = (array, field) => {
+  let mode = [];
+  let mostOccurances = 0;
+  let counter = {};
+  array.forEach(elem => {
+    if (!counter[elem[field]]){
+      counter[elem[field]] = 0
+    }
+    let currOccurances = counter[elem[field]] + 1;
+    counter[elem[field]] = currOccurances;
+    if (currOccurances > mostOccurances) {
+      mostOccurances = currOccurances;
+      mode = [elem[field]];
+    } else if (currOccurances === mostOccurances) {
+      mode.push(elem[field]);
+    }
+  });
+  return mode.length > 1 ? mode : mode[0];
+}
 
 // Takes in an array of posts and the key to find the range of
 // Range is returned as an array of two elements: the smallest value and the largest value
 statistics.range = (array, field) => {
+  // Store the lowest value in the first element of the array and the highest value in the second element
   const range = [array[0][field],array[0][field]];
+  // Iterate through the array to find the lowest and highest values and store them in the range array
   array.forEach(elem => {
     range[0] = Math.min(elem[field], range[0]);
     range[1] = Math.max(elem[field], range[1]);
   })
+  // Return the range array that now contains the lowest value at the 0 index and the highest value at the 1 index
   return range;
 }

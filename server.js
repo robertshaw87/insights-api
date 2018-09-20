a43b07b8ebbeafa32e24865b3367ef1f787673a4
Robert Shaw <robertshaw87@gmail.com>
2018-09-19 23:05:27 -0700

    create initial server file

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require("express");
 
+// Use port 3000 for development
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+// Import API routes from controller
+const routes = require("./controllers/insight_controller.js");
+app.use(routes);
+
+// Set the server to start listening on our PORT
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`));

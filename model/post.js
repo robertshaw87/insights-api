3e9151909b104c5e6aadb8dff1519959679849c9
Robert Shaw <robertshaw87@gmail.com>
2018-09-20 00:02:26 -0700

    add moment for time math

--- model/post.js
+++ model/post.js
@@ -3,8 +3,12 @@ const DATA = require("./testData.json").data;
 const posts = module.exports = {};
 
 posts.getAll = function(options) {
-  const returnData = [...DATA]
+  const returnData = [...DATA];
   const maxEntries = isNaN(options.limit) ? DATA.length : options.limit;
-  returnData.splice(maxEntries)
+  switch (options.sort) {
+    case "asc":
+    case "desc":
+  }
+  returnData.splice(maxEntries);
   return returnData;
 }

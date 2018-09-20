62a375d905eff87c30b6ed834f93af97cfec88c0
Robert Shaw <robertshaw87@gmail.com>
2018-09-20 03:48:25 -0700

    add comments to the mode helper

--- utility/statistics.js
+++ utility/statistics.js
@@ -27,22 +27,34 @@ statistics.median = (array, field) => {
 // Takes in an array of posts and the key to find the mode of
 // The mode is returned as either a value or an array of values if there's more than one mode
 statistics.mode = (array, field) => {
+  // Use an array to keep track of the mode by default
   let mode = [];
+  // The value with the most occurances starts off occuring 0 times
   let mostOccurances = 0;
+  // This object will keep track of how many times each value has occurred
   let counter = {};
+  // Iterate through the array
   array.forEach(elem => {
+    // If we've never encountered the value before, instantiate it at 0
     if (!counter[elem[field]]){
       counter[elem[field]] = 0
     }
+    // Increment the number of occurances of the current value in the object
     let currOccurances = counter[elem[field]] + 1;
     counter[elem[field]] = currOccurances;
+    // If the number of occurances of the current value is greater than the most number of occurances so far
     if (currOccurances > mostOccurances) {
+      // Set the most number of occurances to the current number
       mostOccurances = currOccurances;
+      // Reset mode to only contain the current value
       mode = [elem[field]];
+    // If the number of occurances of the current value is equal to the most number of occurances so far
     } else if (currOccurances === mostOccurances) {
+      // Add the current value to the mode output array
       mode.push(elem[field]);
     }
   });
+  // If there's more than one element in the array, return the array; otherwise, return only the value
   return mode.length > 1 ? mode : mode[0];
 }
 
